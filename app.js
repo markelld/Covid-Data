8106d5d16f7af2cfd8e820b1b8599ebc761f813e
“markelld” <markelldehaney@gmail.com>
2020-12-21 11:40:15 -0600

    created project files

---
+++ app.js
@@ -0,0 +1,11 @@
+// step 1  Grab Covid data from API
+
+// step 2 Add Data to Dom after selecting covid id in html  
+
+// step 3 Create remove function (event handler) to get rid of search whenever ner search is done in that container 
+
+// step 4 Grab data for testings sites from API 
+
+// step 5 Add Data to Dom after selecting Testing id in html 
+
+// step 6 Create remove function (event handler) to get rid of search whenever  search is done in that container
